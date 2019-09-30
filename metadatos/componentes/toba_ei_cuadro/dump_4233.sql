03366b34a6c4e397d9c16dc696e73d6a8a34ed1c
Federico Alemany <mfalemany@gmail.com>
2019-09-30 11:05:01 -0300

    Reporte de Admisibilidad Se agregan dos columnas al reporte de Admisibilidad: mail del becario y mail del director

--- metadatos/componentes/toba_ei_cuadro/dump_4233.sql
+++ metadatos/componentes/toba_ei_cuadro/dump_4233.sql
@@ -16,8 +16,8 @@ INSERT INTO apex_objeto (proyecto, objeto, anterior, identificador, reflexivo, c
 	'toba', --clase_proyecto
 	'toba_ei_cuadro', --clase
 	'22', --punto_montaje
-	NULL, --subclase
-	NULL, --subclase_archivo
+	'cu_solicitudes', --subclase
+	'becas/admisibilidad/formularios/cu_solicitudes.php', --subclase_archivo
 	NULL, --objeto_categoria_proyecto
 	NULL, --objeto_categoria
 	'ci_admisibilidad - cu_solicitudes', --nombre
