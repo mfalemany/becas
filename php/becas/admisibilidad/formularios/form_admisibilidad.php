ac51a677eb6d0c2f7edabbf3ec71a9ae9a226d24
Federico Alemany <mfalemany@gmail.com>
2019-10-18 08:26:08 -0300

    Admisibilidad Se agrega la posibilidad de ver el CVAr

--- php/becas/admisibilidad/formularios/form_admisibilidad.php
+++ php/becas/admisibilidad/formularios/form_admisibilidad.php
@@ -7,22 +7,51 @@ class form_admisibilidad extends becas_ei_formulario
 		$insc = $this->controlador()->get_datos('inscripcion','inscripcion_conv_beca')->get();
 		$conv = toba::consulta_php('co_convocatoria_beca')->get_campo('convocatoria',$insc['id_convocatoria']);
 		$tipo_beca = toba::consulta_php('co_tipos_beca')->get_campo('tipo_beca',$insc['id_tipo_beca']);
+		
+		$base = "/documentos/";
+
+		//ANALITICO
 		if($insc['archivo_analitico']){
-			$base = "/documentos/";
 			$ruta = $base.'becas/doc_por_convocatoria/'.$conv."/".$tipo_beca."/".$insc['nro_documento']."/";
 			echo "<td><a class='enlace_boton' href='".$ruta.'Cert. Analitico.pdf'."' target='_BLANK'>Ver analítico</a></td>";
 		}
 
-		$base = toba::consulta_php('helper_archivos')->ruta_base();
+		//COPIA DNI
 		$archivo = 'docum_personal/'.$insc['nro_documento'].'/dni.pdf';
-
 		if(file_exists($base.$archivo)){
 			echo "<td><a class='enlace_boton' href='/documentos/".$archivo."' target='_BLANK'>Ver DNI</a></td>";
 		}
 
+		//INSCRIPCION A POSGRADO
 		if($insc['archivo_insc_posgrado']){
 			echo "<td><a href='".$ruta."Insc. o Compromiso Posgrado.pdf"."' class='enlace_boton' target='_BLANK'>Insc./Compromiso Posgrado</a></td>";
-		}	
+		}
+
+		echo "</tr><tr>";
+		//================= SECCION CVAR DIRECTORES ===================================
+		$ruta = toba::consulta_php('helper_archivos')->ruta_base();
+		$archivo = 'docum_personal/'.$insc['nro_documento_dir'].'/cvar.pdf';
+		if(file_exists($ruta.$archivo)){
+			echo "<td><a class='enlace_boton' href='/documentos/".$archivo."' target='_BLANK'>CVAr Director</a></td>";
+		}
+
+		if(isset($insc['nro_documento_codir'])){
+			$archivo = 'docum_personal/'.$insc['nro_documento_codir'].'/cvar.pdf';
+			if(file_exists($ruta.$archivo)){
+				echo "<td><a class='enlace_boton' href='/documentos/".$archivo."' target='_BLANK'>CVAr Co-Director</a></td>";
+			}
+		}
+
+		if(isset($insc['nro_documento_subdir'])){
+			$archivo = 'docum_personal/'.$insc['nro_documento_subdir'].'/cvar.pdf';
+			if(file_exists($ruta.$archivo)){
+				echo "<td><a class='enlace_boton' href='/documentos/".$archivo."' target='_BLANK'>CVAr Sub-Director</a></td>";
+			}
+		}
+
+
+
+
 		echo "</tr></table>";
 		
 		$this->generar_html_ef('prom_hist');
