1f0fe1dba63e0bce1df9b10ef80353a5f2eb6621
Federico Alemany <mfalemany@gmail.com>
2020-02-19 09:17:55 -0300

    Becas: Reporte para resolucion Se agregan las columnas 'Mail del Becario' y 'Mail del Director'

--- metadatos/componentes/toba_ei_cuadro/dump_4527.sql
+++ metadatos/componentes/toba_ei_cuadro/dump_4527.sql
@@ -95,7 +95,7 @@ INSERT INTO apex_objeto_ei_cuadro_columna (objeto_cuadro_proyecto, objeto_cuadro
 	'4527', --objeto_cuadro
 	'1960', --objeto_cuadro_col
 	'becario', --clave
-	'1', --orden
+	'2', --orden
 	'Apellido y Nombres', --titulo
 	'ei-cuadro-col-tit', --estilo_titulo
 	'4', --estilo
@@ -126,7 +126,7 @@ INSERT INTO apex_objeto_ei_cuadro_columna (objeto_cuadro_proyecto, objeto_cuadro
 	'4527', --objeto_cuadro
 	'1961', --objeto_cuadro_col
 	'becario_cuil', --clave
-	'2', --orden
+	'1', --orden
 	'CUIL', --titulo
 	'ei-cuadro-col-tit', --estilo_titulo
 	'4', --estilo
@@ -157,7 +157,7 @@ INSERT INTO apex_objeto_ei_cuadro_columna (objeto_cuadro_proyecto, objeto_cuadro
 	'4527', --objeto_cuadro
 	'1962', --objeto_cuadro_col
 	'titulo_plan_beca', --clave
-	'3', --orden
+	'6', --orden
 	'Título del Plan de Beca', --titulo
 	'ei-cuadro-col-tit', --estilo_titulo
 	'4', --estilo
@@ -219,7 +219,7 @@ INSERT INTO apex_objeto_ei_cuadro_columna (objeto_cuadro_proyecto, objeto_cuadro
 	'4527', --objeto_cuadro
 	'1964', --objeto_cuadro_col
 	'codirector', --clave
-	'5', --orden
+	'7', --orden
 	'Co-Director', --titulo
 	'ei-cuadro-col-tit', --estilo_titulo
 	'4', --estilo
@@ -250,7 +250,7 @@ INSERT INTO apex_objeto_ei_cuadro_columna (objeto_cuadro_proyecto, objeto_cuadro
 	'4527', --objeto_cuadro
 	'1965', --objeto_cuadro_col
 	'subdirector', --clave
-	'6', --orden
+	'8', --orden
 	'Sub-Director', --titulo
 	'ei-cuadro-col-tit', --estilo_titulo
 	'4', --estilo
@@ -281,7 +281,7 @@ INSERT INTO apex_objeto_ei_cuadro_columna (objeto_cuadro_proyecto, objeto_cuadro
 	'4527', --objeto_cuadro
 	'1966', --objeto_cuadro_col
 	'area_trabajo', --clave
-	'7', --orden
+	'9', --orden
 	'Área/Dpto/Laboratorio', --titulo
 	'ei-cuadro-col-tit', --estilo_titulo
 	'4', --estilo
@@ -312,7 +312,7 @@ INSERT INTO apex_objeto_ei_cuadro_columna (objeto_cuadro_proyecto, objeto_cuadro
 	'4527', --objeto_cuadro
 	'1967', --objeto_cuadro_col
 	'dependencia', --clave
-	'8', --orden
+	'10', --orden
 	'Facultad/Instituto', --titulo
 	'ei-cuadro-col-tit', --estilo_titulo
 	'4', --estilo
@@ -343,7 +343,7 @@ INSERT INTO apex_objeto_ei_cuadro_columna (objeto_cuadro_proyecto, objeto_cuadro
 	'4527', --objeto_cuadro
 	'1984', --objeto_cuadro_col
 	'tipo_beca', --clave
-	'10', --orden
+	'12', --orden
 	'Tipo de Beca', --titulo
 	'ei-cuadro-col-tit', --estilo_titulo
 	'4', --estilo
@@ -374,8 +374,70 @@ INSERT INTO apex_objeto_ei_cuadro_columna (objeto_cuadro_proyecto, objeto_cuadro
 	'4527', --objeto_cuadro
 	'1985', --objeto_cuadro_col
 	'lugar_trabajo_becario', --clave
-	'9', --orden
+	'11', --orden
 	'Lugar de Trabajo Becario', --titulo
+	'ei-cuadro-col-tit', --estilo_titulo
+	'4', --estilo
+	NULL, --ancho
+	'1', --formateo
+	NULL, --vinculo_indice
+	NULL, --no_ordenar
+	NULL, --mostrar_xls
+	NULL, --mostrar_pdf
+	NULL, --pdf_propiedades
+	NULL, --desabilitado
+	NULL, --total
+	NULL, --total_cc
+	NULL, --usar_vinculo
+	NULL, --vinculo_carpeta
+	NULL, --vinculo_item
+	NULL, --vinculo_popup
+	NULL, --vinculo_popup_param
+	NULL, --vinculo_target
+	NULL, --vinculo_celda
+	NULL, --vinculo_servicio
+	'0', --permitir_html
+	NULL, --grupo
+	NULL  --evento_asociado
+);
+INSERT INTO apex_objeto_ei_cuadro_columna (objeto_cuadro_proyecto, objeto_cuadro, objeto_cuadro_col, clave, orden, titulo, estilo_titulo, estilo, ancho, formateo, vinculo_indice, no_ordenar, mostrar_xls, mostrar_pdf, pdf_propiedades, desabilitado, total, total_cc, usar_vinculo, vinculo_carpeta, vinculo_item, vinculo_popup, vinculo_popup_param, vinculo_target, vinculo_celda, vinculo_servicio, permitir_html, grupo, evento_asociado) VALUES (
+	'becas', --objeto_cuadro_proyecto
+	'4527', --objeto_cuadro
+	'1991', --objeto_cuadro_col
+	'mail_becario', --clave
+	'3', --orden
+	'Mail Becario', --titulo
+	'ei-cuadro-col-tit', --estilo_titulo
+	'4', --estilo
+	NULL, --ancho
+	'1', --formateo
+	NULL, --vinculo_indice
+	NULL, --no_ordenar
+	NULL, --mostrar_xls
+	NULL, --mostrar_pdf
+	NULL, --pdf_propiedades
+	NULL, --desabilitado
+	NULL, --total
+	NULL, --total_cc
+	NULL, --usar_vinculo
+	NULL, --vinculo_carpeta
+	NULL, --vinculo_item
+	NULL, --vinculo_popup
+	NULL, --vinculo_popup_param
+	NULL, --vinculo_target
+	NULL, --vinculo_celda
+	NULL, --vinculo_servicio
+	'0', --permitir_html
+	NULL, --grupo
+	NULL  --evento_asociado
+);
+INSERT INTO apex_objeto_ei_cuadro_columna (objeto_cuadro_proyecto, objeto_cuadro, objeto_cuadro_col, clave, orden, titulo, estilo_titulo, estilo, ancho, formateo, vinculo_indice, no_ordenar, mostrar_xls, mostrar_pdf, pdf_propiedades, desabilitado, total, total_cc, usar_vinculo, vinculo_carpeta, vinculo_item, vinculo_popup, vinculo_popup_param, vinculo_target, vinculo_celda, vinculo_servicio, permitir_html, grupo, evento_asociado) VALUES (
+	'becas', --objeto_cuadro_proyecto
+	'4527', --objeto_cuadro
+	'1992', --objeto_cuadro_col
+	'mail_director', --clave
+	'5', --orden
+	'Mail Director', --titulo
 	'ei-cuadro-col-tit', --estilo_titulo
 	'4', --estilo
 	NULL, --ancho
