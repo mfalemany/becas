3d54aa25eaf5256a1df22a3cf66a295c834dffcd
Federico Alemany <mfalemany@gmail.com>
2020-08-12 19:54:29 -0300

    Carga de Recibos de Sueldo

--- metadatos/permisos/grupo_acceso__admin.sql
+++ metadatos/permisos/grupo_acceso__admin.sql
@@ -280,4 +280,10 @@ INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, i
 	NULL, --item_id
 	'3689'  --item
 );
+INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
+	'becas', --proyecto
+	'admin', --usuario_grupo_acc
+	NULL, --item_id
+	'3698'  --item
+);
 --- FIN Grupo de desarrollo 0
