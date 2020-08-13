db98690eb5b24c26c019e7ac47ea072fcf9b60a7
Federico Alemany <mfalemany@gmail.com>
2020-08-12 22:15:35 -0300

    Recibos de sueldo Se corrigen estilos

--- php/item_inicial.php
+++ php/item_inicial.php
@@ -1,25 +1,27 @@
 <?php
 	$usuario = toba::usuario()->get_id();
-	//$archivos = toba::consulta_php('helper_archivos')->get_recibos_sueldo($usuario);
+	//$archivos = toba::consulta_php('helper_archivos')->get_recibos_sueldo('28666208');
 	$archivos = toba::consulta_php('helper_archivos')->get_recibos_sueldo($usuario);
 	if(count($archivos)){
 
-		echo "<table border=1 width='400px' style='position:absolute; top: 100px; left: 40px; font-size:1.4rem; border-collapse: collapse;'>
-		<caption style='font-size:1.5em; font-weight:bold; color:#4344B9;'> Recibos de sueldo disponibles</caption>
+		echo "<table id='tabla_recibos'>
+		<caption> Recibos de sueldo disponibles</caption>
 		<th>Nro. Recibo</th><th>Fecha</th><th>Descargar</th>";
 		foreach($archivos as $archivo){
 			$partes = explode('_',$archivo);
 			$fecha = str_replace('.pdf','',$partes[2]);
 			
-			echo "<tr><td class='centrado'>{$partes[1]}</td><td  class='centrado'>$fecha</td>";
-			echo "<td  class='centrado'><a style='text-decoration:none;' href='/documentos/recibos_sueldo/$archivo' target='_BLANK' rel='noopener noreferrer'>Click Aquí</a></td>";
+			echo "<tr><td>{$partes[1]}</td><td>$fecha</td>";
+			echo "<td><a href='/documentos/recibos_sueldo/$archivo' target='_BLANK' rel='noopener noreferrer'>Click Aquí</a></td>";
 		}
 		echo "</table>";
 	}
+	echo "<div id='logo_inicio'>";
 	echo '<h1 style="color:#3c84ce; text-shadow: 0px 0px 1px #77A;">Bienvenido</h1>';		
 	echo '<h3 style="color:#222; text-shadow: 0px 0px 1px #777;">Sistema de Gesti&oacute;n de Becas - SGCyT - UNNE</h3>';
 	echo '<div class="logo" style="margin-top:60px">';
 	echo toba_recurso::imagen_proyecto('logo_grande.gif', true);
+	echo '</div>';
 	echo '</div>';
 
 	if( ! in_array('becario',toba::usuario()->get_perfiles_funcionales())){
