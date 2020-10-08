22ae2cbe3da666302677dcdaf138c675a8486a18
Federico Alemany <mfalemany@gmail.com>
2020-10-08 12:20:25 -0300

    Exportación a Excel Se cambia la extensión del formato de salida por .xls

--- php/becas/admisibilidad/formularios/cu_solicitudes.php
+++ php/becas/admisibilidad/formularios/cu_solicitudes.php
@@ -3,7 +3,7 @@ class cu_solicitudes extends becas_ei_cuadro
 {
 	function vista_excel(toba_vista_excel $salida)
 	{
-		$salida->set_nombre_archivo('Admisibilidad de Becas.xlsx');
+		$salida->set_nombre_archivo('Admisibilidad de Becas.xls');
 		$this->agregar_columnas(array(
 			'mail_becario'  => array('clave' => 'mail_becario', 'titulo' => 'Mail Becario'),
 			'mail_director' => array('clave' => 'mail_director','titulo' => 'Mail Director')
