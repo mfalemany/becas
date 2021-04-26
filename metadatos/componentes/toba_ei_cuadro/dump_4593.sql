3b9e9922a204f92fd8f2a2b3dc07bf3862dbaf3f
Federico Alemany <mfalemany@gmail.com>
2021-04-26 11:24:37 -0300

    Becas: presentación de informes Se quita la columna 'Fecha limite de presentación' por generar confusión a los usuarios

--- metadatos/componentes/toba_ei_cuadro/dump_4593.sql
+++ metadatos/componentes/toba_ei_cuadro/dump_4593.sql
@@ -197,43 +197,12 @@ INSERT INTO apex_objeto_ei_cuadro_columna (objeto_cuadro_proyecto, objeto_cuadro
 	'4593', --objeto_cuadro
 	'2047', --objeto_cuadro_col
 	'estado', --clave
-	'4', --orden
+	'3', --orden
 	'Estado', --titulo
 	'ei-cuadro-col-tit', --estilo_titulo
-	'4', --estilo
+	'col-cen-s1', --estilo
 	NULL, --ancho
 	'1', --formateo
-	NULL, --vinculo_indice
-	NULL, --no_ordenar
-	NULL, --mostrar_xls
-	NULL, --mostrar_pdf
-	NULL, --pdf_propiedades
-	NULL, --desabilitado
-	NULL, --total
-	NULL, --total_cc
-	NULL, --usar_vinculo
-	NULL, --vinculo_carpeta
-	NULL, --vinculo_item
-	NULL, --vinculo_popup
-	NULL, --vinculo_popup_param
-	NULL, --vinculo_target
-	NULL, --vinculo_celda
-	NULL, --vinculo_servicio
-	'0', --permitir_html
-	NULL, --grupo
-	NULL  --evento_asociado
-);
-INSERT INTO apex_objeto_ei_cuadro_columna (objeto_cuadro_proyecto, objeto_cuadro, objeto_cuadro_col, clave, orden, titulo, estilo_titulo, estilo, ancho, formateo, vinculo_indice, no_ordenar, mostrar_xls, mostrar_pdf, pdf_propiedades, desabilitado, total, total_cc, usar_vinculo, vinculo_carpeta, vinculo_item, vinculo_popup, vinculo_popup_param, vinculo_target, vinculo_celda, vinculo_servicio, permitir_html, grupo, evento_asociado) VALUES (
-	'becas', --objeto_cuadro_proyecto
-	'4593', --objeto_cuadro
-	'2048', --objeto_cuadro_col
-	'fecha_debe_ser_presentado', --clave
-	'3', --orden
-	'Fecha límite de presentación', --titulo
-	'ei-cuadro-col-tit', --estilo_titulo
-	'col-cen-s1', --estilo
-	'120px', --ancho
-	'8', --formateo
 	NULL, --vinculo_indice
 	'0', --no_ordenar
 	NULL, --mostrar_xls
