9f6162ef51a71f98460671f14bd6d45e06cd1b30
Federico Alemany <mfalemany@gmail.com>
2021-05-10 11:30:28 -0300

    Reporte de Becas se corrige exportación a excel que salía vacío

--- php/becas/reportes/ci_reporte_general.php
+++ php/becas/reportes/ci_reporte_general.php
@@ -1,15 +1,15 @@
 <?php
 class ci_reporte_general extends becas_ci
 {
-	protected $filtro;
+	protected $s__filtro;
 	//-----------------------------------------------------------------------------------
 	//---- cu_becas ---------------------------------------------------------------------
 	//-----------------------------------------------------------------------------------
 
 	function conf__cu_becas(becas_ei_cuadro $cuadro)
 	{
-		if(isset($this->filtro)){
-			$cuadro->set_datos(toba::consulta_php('co_becas_otorgadas')->get_becas_otorgadas($this->filtro));
+		if(isset($this->s__filtro)){
+			$cuadro->set_datos(toba::consulta_php('co_becas_otorgadas')->get_becas_otorgadas($this->s__filtro));
 		}else{
 			$cuadro->set_titulo('Debe filtrar para ver resultados en este cuadro');
 		}
@@ -21,19 +21,19 @@ class ci_reporte_general extends becas_ci
 
 	function conf__filtro(becas_ei_formulario $form)
 	{
-		if(isset($this->filtro)){
-			$form->set_datos($this->filtro);
+		if(isset($this->s__filtro)){
+			$form->set_datos($this->s__filtro);
 		}
 	}
 
 	function evt__filtro__filtrar($datos)
 	{
-		$this->filtro = $datos;
+		$this->s__filtro = $datos;
 	}
 
 	function evt__filtro__cancelar()
 	{
-		unset($this->filtro);
+		unset($this->s__filtro);
 	}
 
 }
